90f51f2135e93c5869cac383486eb29bb4ef9bad
Atemkln <adambeach16@gmail.com>
2020-12-29 16:10:16 -0500

    update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    private Animator playeranim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playeranim = GetComponent<Animator>();
+        playeranim.Play
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+    }
+}
